b603f2a377040a5731e1a46af75c552efb0729a9
Matthew Wiessing <matthewwiessing@gmail.com>
2021-09-06 16:31:21 -0500

    changed api handles

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
-var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var geoCodingUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
+var oneCall = "https://api.openweathermap.org/data/2.5/onecall?";
 var APIkey = "&appid=847614a96bad4bae8518be6e90117639";
-
+var exclude = "&exclude=hourly,minutely,alerts";
 var searchBtn = document.querySelector("#search-btn");
-searchBtn.addEventListener("click", searchApi);
-function searchApi() {
+let lat;
+let lon;
+
+searchBtn.addEventListener("click", searchGeo);
+function searchGeo() {
   var searchInputVal = document.querySelector("#search-input").value;
-  fetch(weatherUrl + searchInputVal + ",us" + "&units=imperial" + APIkey)
+  fetch(geoCodingUrl + searchInputVal + ",us" + "&units=imperial" + APIkey)
     .then((response) => {
+      //   console.log(response);
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      lat = data[0].lat;
+      lon = data[0].lon;
+      console.log(lat, lon);
+
+      searchOneCall();
+    });
+}
+function showResults() {
+  var resultCard = document.createElement("div");
+  resultCard.classList.add("card", "bg-light", "text-dark", "mb-3", "p-3");
+
+  var resultBody = document.createElement("div");
+  resultBody.classList.add("card-body");
+
+  var bodyContentEl = document.createElement("p");
+  bodyContentEl.textContent = (lat, lon).current.weather.icon;
+
+  var titleEl = document.createElement("h3");
+  titleEl.textContent = city.name;
+  resultBody.appendChild(titleEl);
+  resultCard.appendChild(resultBody);
+  document.body.appendChild(resultCard);
+}
+function searchOneCall() {
+  fetch(
+    oneCall +
+      "lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=imperial" +
+      exclude +
+      APIkey
+  )
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      showResults();
     });
 }
