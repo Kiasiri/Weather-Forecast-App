bdfb559c5d3eb3d8400a58561b34eacd370eca32
Matthew Wiessing <matthewwiessing@gmail.com>
2021-09-06 22:27:11 -0500

    dates display

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function searchGeo() {
   var searchInputVal = document.querySelector("#search-input").value;
   fetch(geoCodingUrl + searchInputVal + ",us" + "&units=imperial" + APIkey)
     .then((response) => {
-      //   console.log(response);
       return response.json();
     })
     .then(function (data) {
@@ -24,12 +23,62 @@ function searchGeo() {
     });
 }
 function showResults(data) {
-  for (let i = 0; i < 5; i++) {
-    var resultCard = document.createElement("div");
-    resultCard.classList.add("card", "bg-light", "text-dark", "mb-3", "p-3");
+  var currentCard = document.createElement("div");
+  currentCard.classList.add("card", "bg-primary", "text-light", "mb-2", "p-2");
+
+  var resultBody = document.createElement("div");
+  resultBody.classList.add("card-body");
+
+  var date = document.querySelector("#search-input").value;
+  var futureDate = document.createElement("h3");
+
+  var icon = document.createElement("img");
+  icon.src =
+    "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png";
+
+  var windSpeed = document.createElement("p");
+  windSpeed.textContent = "Windspeed:" + data.current.wind_speed + "mph";
+
+  var temprature = document.createElement("p");
+  temprature.textContent =
+    "Temprature: Max " +
+    data.current.temp +
+    "°F" +
+    ", " +
+    "feels Like " +
+    data.current.feels_like +
+    "°F";
+
+  var humidity = document.createElement("p");
+  humidity.textContent = "Humidity:" + data.current.humidity + "%";
+  var currentDate = document.createElement("h3");
+  var month = new Date().getMonth();
+  var date = new Date().getDate();
+  currentDate.textContent = month + "/" + date;
+  document.body.appendChild(currentCard);
+  currentCard.appendChild(currentDate);
+  currentCard.appendChild(icon);
+  currentCard.appendChild(temprature);
+  currentCard.appendChild(windSpeed);
+  currentCard.appendChild(humidity);
+  resultBody.appendChild(currentCard);
+  document.body.appendChild(currentCard);
+
+  for (let i = 0; i <= 4; i++) {
+    var forecastCard = document.createElement("div");
+    forecastCard.classList.add(
+      "card",
+      "bg-success",
+      "text-light",
+      "mb-2",
+      "p-2"
+    );
 
     var resultBody = document.createElement("div");
     resultBody.classList.add("card-body");
+
+    var date = document.querySelector("#search-input").value;
+    var futureDate = document.createElement("h3");
 
     var icon = document.createElement("img");
     icon.src =
@@ -37,16 +86,32 @@ function showResults(data) {
       data.daily[i].weather[0].icon +
       ".png";
 
-    var bodyContentEl = document.createElement("p");
-    bodyContentEl.textContent = data.daily[i].weather[0].description;
+    var windSpeed = document.createElement("p");
+    windSpeed.textContent = "Windspeed:" + data.daily[i].wind_speed + "mph";
 
-    var titleEl = document.createElement("h3");
-    //titleEl.textContent = city.name;
-    resultBody.appendChild(titleEl);
-    resultCard.appendChild(icon);
-    resultCard.appendChild(bodyContentEl);
-    resultCard.appendChild(resultBody);
-    document.body.appendChild(resultCard);
+    var temprature = document.createElement("p");
+    temprature.textContent =
+      "Temprature: Max" +
+      data.daily[i].temp.max +
+      "°F" +
+      ", " +
+      "Min" +
+      data.daily[i].temp.min +
+      "°F";
+
+    var humidity = document.createElement("p");
+    humidity.textContent = "Humidity:" + data.daily[i].humidity + "%";
+    var date = new Date().getDate();
+    var month = new Date().getMonth();
+    futureDate.textContent = month + "/" + (date + i + 1);
+    forecastCard.appendChild(futureDate);
+    forecastCard.appendChild(icon);
+    forecastCard.appendChild(temprature);
+    forecastCard.appendChild(windSpeed);
+    forecastCard.appendChild(humidity);
+    //forecastCard.appendChild(resultBody);
+    resultBody.appendChild(forecastCard);
+    document.body.appendChild(forecastCard);
   }
 }
 function searchOneCall() {
